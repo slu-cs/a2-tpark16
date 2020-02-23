c31210cc4404746e7d71b24c66c80412e43c7a1a
tpark16 <tpark16@stlawu.edu>
2020-02-23 17:31:17 -0500

    1

---
+++ schema.js
@@ -0,0 +1,21 @@
+// Define a plan for a collection
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of professors
+
+const Voters = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  zipCode: Number,
+  history: String
+});
+
+
+// Spped up queries on all fields
+Voters.index({firstName: 1});
+Voters.index({lastName: 1});
+Voters.index({zipCode: 1});
+Voters.index({history: 1});
+
+module.exports = mongoose.model('Voters', Voters);
