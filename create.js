fe99cf10d6ac410716e83a0aee5ebba50c451e9c
tpark16 <tpark16@stlawu.edu>
2020-02-23 17:55:47 -0500

    1

--- create.js
+++ create.js
@@ -19,7 +19,7 @@ user.question('Filename: ', function(filename) {
 
   // Asynchronous line-by-line input
   file.on('line', function(line) {
-    
+    console.log(line[0])
   });
 
   // End the program when the file closes
