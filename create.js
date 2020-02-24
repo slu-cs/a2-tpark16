8c40f9635fa7b7490017a55584e8f018411fe6ea
tpark16 <tpark16@stlawu.edu>
2020-02-23 20:57:07 -0500

    1

--- create.js
+++ create.js
@@ -26,12 +26,13 @@ user.question('Filename: ', function(filename) {
   // Asynchronous line-by-line input
   file.on('line', function(line) {
     var splited_line = line.split(/,/);
-    const value = new Voters({
-      firstName: splited_line[0],
-      lastName: splited_line[1],
-      zipCode: splited_line[2],
-      history: splited_line[3]
-    })
+    console.log(splited_line);
+    // const value = new Voters({
+    //   firstName: splited_line[0],
+    //   lastName: splited_line[1],
+    //   zipCode: splited_line[2],
+    //   history: splited_line[3]
+    // })
 
   });
 
