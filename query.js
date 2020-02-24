ed48588ec1cb4e1b3b339a721763096187c779f2
tpark16 <tpark16@stlawu.edu>
2020-02-23 23:56:59 -0500

    1

--- query.js
+++ query.js
@@ -6,7 +6,11 @@ const Voters = require('./schema');
 connect(); // To the database
 
 const query = [
-  Voters.find()
+    Voters.find().where('zipcode').equals(13617),
+    Voters.find().where('first_name').equals("STARR"),
+    Voters.where('history').in('GE16').count()),
+    Voters.find().sort("-last_name").limit(1),
+    Voters.distinct('zipcode')
 ];
 
 // query.exec(function(error, voters) {
@@ -17,6 +21,10 @@ const query = [
 // Run the queries in parallel
 Promise.all(query)
   .then(function(results) {
-    console.log('Names in order: ', results[0]);
+    console.log('The number of registered voters that live in zip code 13617: ', results[0]);
+    console.log('The full names of all of the registered voters whose first-name is STARR: ', results[1].map(v => v.firstName + ' ' + v.lastName));
+    console.log('Number of people who voted in the GE 2016: ', results[2]);
+    console.log('The last-name that comes in last in the county in alphabetical order:  ', results[3].map(v => v.lastName));
+    console.log('The number of zip codes that the county contains: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
