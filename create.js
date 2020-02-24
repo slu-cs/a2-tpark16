fa318573ab4390895c31b73592094ba695d095cc
tpark16 <tpark16@stlawu.edu>
2020-02-23 23:33:31 -0500

    1

--- create.js
+++ create.js
@@ -8,41 +8,26 @@ const Voters = require('./schema');
 
 connect(); // To the database
 
-// Console configuration
-const user = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 let voter = [];
-// Console input
-user.question('Filename: ', function(filename) {
 
-
-  // File configuration
-  const file = readline.createInterface({
-    input: fs.createReadStream(filename)
-  });
-
-  // Asynchronous line-by-line input
-  file.on('line', function(line) {
-    var splited_line = line.split(/,/);
-    voter.push(new Voters({
-      firstName: splited_line[0],
-      lastName: splited_line[1],
-      zipCode: splited_line[2],
-      history: splited_line[3]
-    }));
-  });
-
-  mongoose.connection.dropDatabase()
-    .then(() => voter.save())
-    .then(() => mongoose.connection.close())
-    .then(() => console.log('Database is ready.'))
-    .catch(error => console.error(error.stack));
-
-  // End the program when the file closes
-  file.on('close', function() {
-    process.exit(0);
-  });
+// File configuration
+const file = readline.createInterface({
+  input: fs.createReadStream(filename)
 });
+
+// Asynchronous line-by-line input
+file.on('line', function(line) {
+  var splited_line = line.split(/,/);
+  voter.push(new Voters({
+    firstName: splited_line[0],
+    lastName: splited_line[1],
+    zipCode: splited_line[2],
+    history: splited_line[3]
+  }));
+});
+
+mongoose.connection.dropDatabase()
+  .then(() => voter.save())
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
